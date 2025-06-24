c1e689154424cbdf7deaa7998a5109ee7d732858
MattWai <39748665+Mattwai@users.noreply.github.com>
2025-06-24 17:10:06 +1200

    Style(Metadata): update metadata titles and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,19 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "50project-challenge",
+  description: "50 B2B Saas Projects in 50 Days",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
